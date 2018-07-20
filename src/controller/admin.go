97ff22bc8bee7ac13b1f0f99a19511a5e8711bcb
Kis Tamás <kistamas00@freemail.hu>
2018-07-20 19:35:43 +0200

    Add new APs

--- src/controller/admin.go
+++ src/controller/admin.go
@@ -10,11 +10,13 @@ func AddAdminAP(router *gin.Engine)  {
 
 	authorized := router.Group("/admin", gin.BasicAuth(db.GetUsers()))
 
+	// HTML
 	authorized.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "admin.html", nil)
 	})
-	
+
+	// JSON
 	authorized.GET("/votes", func(c *gin.Context) {
-		c.JSON(http.StatusOK, db.GetVotes())
+		c.JSON(http.StatusOK, db.GetVotes(nil))
 	})
 }
